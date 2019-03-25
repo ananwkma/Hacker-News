4ac975d95fb4c8efa43fe63dba2ab0c25de7857a
Anan <anan.w.ma@gmail.com>
2019-03-25 15:39:16 -0700

    Bootstrap App
    - App.js added bootstrap container

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/App.css';
 import Home from './Home'
 import { BrowserRouter as Router } from 'react-router-dom'
